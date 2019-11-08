ee3409eb1c2f7502bbfc0e509973fa88439929c3
TheZoq2 <frans.skarman@protonmail.com>
2019-11-08 20:31:14 +0100

    Add missing server code

--- src/server.rs
+++ src/server.rs
@@ -1,24 +1,46 @@
+use std::io;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::collections::HashMap;
+use serde_json;
+
+mod messages;
+use messages::Message;
+
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:30000").unwrap();
+    let mut connections = vec!();
+
+    let mut listener = TcpListener::bind("127.0.0.1:30000")
+        .unwrap();
+
+    listener.set_nonblocking(true);
+
     println!("Listening on 127.0.0.1:30000");
 
-    let mut player1_stream = listener.incoming().next().unwrap().unwrap();
-    println!("First player connected");
+    let mut next_id = 0;
+    loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    println!("Got new connection {}", next_id);
+                    connections.push((next_id, stream));
+                    next_id += 1;
+                }
+                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                    // wait until network socket is ready, typically implemented
+                    // via platform-specific APIs such as epoll or IOCP
+                    break;
+                }
+                e => {e.expect("Socket listener error");}
+            }
+        }
 
-    let mut player2_stream = listener.incoming().next().unwrap().unwrap();
-    println!("Second player connected");
-
-    let mut buffer = [0; 512];
-    player1_stream.read(&mut buffer).unwrap();
-    println!("Received data from player1: {}", String::from_utf8_lossy(&buffer[..]));
-
-    player2_stream.read(&mut buffer).unwrap();
-    println!("Received data from player2: {}", String::from_utf8_lossy(&buffer[..]));
-
-    player1_stream.write("100.0 200.0\0".as_bytes()).unwrap();
-    player2_stream.write("300.0 50.0\0".as_bytes()).unwrap();
+        for (id, ref mut client) in connections.iter_mut() {
+            client.write(serde_json::to_string(&Message::Ping).expect("Failed to encode message").as_bytes())
+                .expect("Failed to send message to client");
+        }
+    }
 }
+
