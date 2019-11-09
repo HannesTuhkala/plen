635735eab6e83935c5e73a6396c33d506c00a6f4
Hannes Tuhkala <hantu447@student.liu.se>
2019-11-09 21:10:42 +0100

    fixed jkdak la

--- src/map.rs
+++ src/map.rs
@@ -151,7 +151,7 @@ impl Map {
         }
 
         let position = na::Point2::new(
-            constants::WINDOW_SIZE - 550.,
+            constants::WINDOW_SIZE - 530.,
             -constants::WINDOW_SIZE/2.);
         yeehaw_sb.add(
             graphics::DrawParam::default()
