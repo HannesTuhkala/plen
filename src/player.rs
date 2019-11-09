06d4875fa965d3209fdba23046cf0f763617d806
Hannes Tuhkala <hantu447@student.liu.se>
2019-11-09 16:42:25 +0100

    Added a PlaneType

--- src/player.rs
+++ src/player.rs
@@ -12,6 +12,43 @@ use rand::Rng;
 use crate::powerups::PowerUpKind;
 
 #[derive(Serialize, Deserialize, Clone)]
+enum PlaneType {
+    SUKA_BLYAT_PLANE,
+    HOWDY_COWBOY_PLANE,
+    EL_POLLO_ROMERO_PLANE,
+    ACHTUNG_BLITZ_PLANE,
+}
+
+impl PlaneType {
+    pub fn speed() -> f64 {
+        match PlaneType {
+            PlaneType::SUKA_BLYAT_PLANE => 20.,
+            PlaneType::HOWDY_COWBOY_PLANE => 10.,
+            PlaneType::EL_POLLO_ROMERO_PLANE => 5.,
+            PlaneType::ACHTUNG_BLITZ_PLANE => 15.,
+        }
+    }
+
+    pub fn agility() -> f64 {
+        match PlaneType {
+            PlaneType::SUKA_BLYAT_PLANE => 3.,
+            PlaneType::HOWDY_COWBOY_PLANE => 4.,
+            PlaneType::EL_POLLO_ROMERO_PLANE => 2.,
+            PlaneType::ACHTUNG_BLITZ_PLANE => 5.,
+        }
+    }
+
+    pub fn firepower() -> f64 {
+        match PlaneType {
+            PlaneType::SUKA_BLYAT_PLANE => 300.,
+            PlaneType::HOWDY_COWBOY_PLANE => 200.,
+            PlaneType::EL_POLLO_ROMERO_PLANE => 200.,
+            PlaneType::ACHTUNG_BLITZ_PLANE => 270.,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Player {
     pub id: u64,
     pub rotation: f32,
@@ -20,6 +57,7 @@ pub struct Player {
     pub position: na::Point2<f32>,
     pub velocity: na::Vector2<f32>,
     pub powerups: Vec<PowerUpKind>,
+    pub planetype: PlaneType,
 }
 
 
@@ -33,6 +71,7 @@ impl Player {
             position: na::Point2::new(100.0, 100.0),
             velocity: na::Vector2::new(0.0, 0.0),
             powerups: vec!(),
+            planetype: PlaneType::SUKA_BLYAT_PLANE,
         }
     }
 
