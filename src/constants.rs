86ed680592ec81b589bbbb13cab8878f1c9a31d9
TheZoq2 <frans.skarman@gmail.com>
2019-11-09 22:25:58 +0100

    Adjust damage and bullet lifetime

--- src/constants.rs
+++ src/constants.rs
@@ -14,8 +14,8 @@ pub const DEFAULT_AGILITY: f32 = 1.;
 pub const ANGULAR_FADE: f32 = 0.9;
 
 pub const BULLET_VELOCITY: f32 = 350.0;
-pub const BULLET_DAMAGE: u8 = 10;
-pub const BULLET_MAX_TRAVEL: f32 = WORLD_SIZE * 0.8;
+pub const BULLET_DAMAGE: u8 = 5;
+pub const BULLET_MAX_TRAVEL: f32 = WORLD_SIZE * 0.3;
 pub const BULLET_START: f32 = 30.;
 pub const PLAYER_COOLDOWN: f32 = 0.2;
 pub const BULLET_ARM_TIME: f32 = 0.06;
