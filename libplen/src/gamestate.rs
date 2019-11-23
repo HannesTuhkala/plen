cbc2952f35fac4230df5d1c8831c281750aacb56
Hannes Tuhkala <hantu447@student.liu.se>
2019-11-23 16:38:43 +0100

    unfucked the unfucked changes

--- libplen/src/gamestate.rs
+++ libplen/src/gamestate.rs
@@ -158,7 +158,7 @@ impl GameState {
                 (laser.angle + std::f32::consts::PI / 2.).sin()
             );
 
-            let hit_radius = PLANE_SIZE + 10;
+            let hit_radius = PLANE_SIZE + 20;
             for player in &mut self.players {
                 if player.id == laser.owner {
                     continue
